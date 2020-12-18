e16454c2ff5c7c0c9a26d5a17e646483c7794b19
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2020-12-18 15:11:15 +0100

    Map output visualisation

--- mapGen/interval.js
+++ mapGen/interval.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const http = require('http');
+const WebSocket = require('websocket');
+const childProcess = require('child_process');
 var input = require('./input.json');
 const filename = "polygonBasedGeneration";
-var prevMapGen = fs.readFileSync(`./${filename}.js`).toString();
+var prevMapGen;
+var prevInput;
 setInterval(() => {
     var curr = fs.readFileSync(`./${filename}.js`).toString();
-    if (prevMapGen == curr) return;
-    prevMapGen = curr;
-    console.log("Reevaluating...");
+    invalidateRequireCacheForFile(`./input.json`);
     input = require('./input.json');
+    if (prevMapGen == curr && prevInput == input) return;
+    prevMapGen = curr;
+    prevInput = input;
+    console.log("Reevaluating...");
     invalidateRequireCacheForFile(`./${filename}.js`);
     try {
         mapGen = require(`./${filename}.js`);
@@ -21,10 +27,50 @@ setInterval(() => {
         fs.writeFileSync("./output.json", JSON.stringify(output));
     } catch (e) {
         console.log("Error with output write.");
-        return;
+    }
+    for (var c of wsConnections) {
+        if (!c) continue;
+        console.log("Sending output...")
+        c.sendUTF(JSON.stringify({
+            fieldSize: 50,
+            map: output
+        }))
     }
     console.log("Reevaluated.");
 }, 300);
 var invalidateRequireCacheForFile = function (filePath) {
     delete require.cache[path.resolve(filePath)];
 };
+
+//HTTP SERVER FOR LIVE PREVIEW OF OUTPUT
+/**
+ * @type {Array<WebSocket.connection>}
+ */
+var wsConnections = [];
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream("./mapshow.html").pipe(res);
+});
+const wsServer = new WebSocket.server({
+    httpServer: httpServer
+});
+wsServer.on("request", request => {
+    var connection = request.accept("echo-protocol", request.origin);
+    wsConnections.push(connection);
+    connection.on("close", () => {
+        delete wsConnections.find((n) => {
+            return n === connection;
+        });
+    });
+    connection.on("message", message => {
+        connection.sendUTF("Method not supported. Use websocket only for updates.");
+    });
+    connection.on("open", () => {
+        connection.sendUTF(JSON.stringify({
+            fieldSize: 50,
+            map: output
+        }))
+    })
+});
+httpServer.listen(5050);
+childProcess.exec("start http://localhost:5050");
